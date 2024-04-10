542a243f51a8b9dd50fa9e024aa0261b2142b0bc
Aryan Soni <arpitsoni811@gmail.com>
2024-04-10 15:34:37 +0530

    tic tac toe done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Square({ value, onSquareClick }) {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={onSquareClick}
+        className="border-black border border-solid h-10 w-10 box-border"
+      >
+        {value}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [square, setSquare] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = square.slice();
+
+    if (nextSquares[i] || winner) return;
+
+    if (xIsNext) {
+      nextSquares[i] = "x";
+      setXIsNext(false);
+    } else {
+      nextSquares[i] = "o";
+      setXIsNext(true);
+    }
+
+    setSquare(nextSquares);
+  }
+  let winner = calculateWinner(square);
+  let boardStatus;
+  if (winner) {
+    boardStatus = "Winner: " + winner;
+  } else {
+    boardStatus = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  return (
+    <>
+      <div>{boardStatus}</div>
+      <div className=" p-4">
+        <div className=" row-auto">
+          <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className=" row-auto">
+          <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={square[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className=" row-auto">
+          <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={square[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div>
+    </>
+  );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        console.log(squares[a], "is the winner.");
+        return squares[a];
+      }
+    }
+    return null;
+  }
+}
